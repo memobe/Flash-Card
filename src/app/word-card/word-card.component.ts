12c132699f343f32e3fbcedd5c32e625ce8870de
memobe <mehranmb78@gmail.com>
2023-05-04 14:27:19 +0200

    feat: add tailwind to project

--- src/app/word-card/word-card.component.ts
+++ src/app/word-card/word-card.component.ts
@@ -1,11 +1,12 @@
 import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Word} from "@flash-card/libs/fc-model";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'flash-card-word-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './word-card.component.html',
   styleUrls: ['./word-card.component.scss'],
 })
